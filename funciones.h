bc7b57f14bbdef9baa7b9f69bb95d7ad4a2c4255
Mario Dueñas <mario.duenas4112@alumnos.udg.mx>
2020-09-21 21:39:50 -0500

    implementamos la suma() y la resta()

--- funciones.h
+++ funciones.h
@@ -1,12 +1,12 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
-void suma (int a, int b){
-
+int suma (int a, int b){
+    return a+b;
 }
 
-void resta (int a, int b){
-
+int resta (int a, int b){
+    return a-b;
 }
 
 #endif // FUNCIONES_H_INCLUDED
